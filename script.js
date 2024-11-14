06fd0046c8ff2556653b08b6d206cfc8d94016d8
RobertoFloris <florisroberto99@gmail.com>
2024-11-14 15:50:27 +0100

    [add] variables

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const multiple3 = "Fizz";
+const multiple5 = "Buzz";
+
+const num1 = 3;
+const num2 = 5;
+
 
 for(let i=1; i<=100; i++){
 
-  if((!(i%3)) && (!(i%5))){
-    console.log("FizzBuzz")
-  } else if(!(i%3)){
-    console.log("Fizz")
-  } else if(!(i%5)){
-    console.log("Buzz")
+  if((!(i%num1)) && (!(i%num2))){
+    console.log(multiple3 + multiple5)
+  } else if(!(i%num1)){
+    console.log(multiple3)
+  } else if(!(i%num2)){
+    console.log(multiple5)
   } else {
     console.log(i)
   }
